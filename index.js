216466a164678db30efddc8118daae67ff6e47e0
rillyy <brilliani.jeshia2812@gmail.com>
2024-12-18 21:30:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -52,3 +52,22 @@ app.get("/", (req, res) => {
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const absensiRoutes = require('./routes/absensi');
+
+const index = express();
+const PORT = 5000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api/absensi', absensiRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
